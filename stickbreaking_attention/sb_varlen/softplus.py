bf51a75c7ca52bfb0ce15997bc0f5e583be0c794
Shawn Tan <shawn@wtf.sg>
2024-12-09 16:53:18 +0000

    Templated ASM generation
    Forward pass:
    triton v torch:
        length  Stickbreaking  Flash Attention
    0   4096.0       1.317595         1.376882
    1   8192.0       4.934436         3.610170
    2  12288.0      11.005516         6.878610
    3  16384.0      19.487679        11.533554

---
+++ stickbreaking_attention/sb_varlen/softplus.py
@@ -0,0 +1,47 @@
+import triton 
+from triton import language as tl
+
+def _generate_asm(num_pack):
+    template = """
+            .reg .pred p;
+            .reg .pred q;
+            .reg .pred r;
+            setp.gt.f32  p, ${in_reg}, 15.;
+            setp.lt.f32  q, ${in_reg}, -15.;
+            or.pred      r, p, q;
+            @p  mov.f32  ${out_reg}, ${in_reg};       // >  15 return x
+            @q  mov.f32  ${out_reg}, 0.;              // < -15 return 0
+            @!r ex2.approx.ftz.f32 ${out_reg}, ${in_reg}; // otherwise log(exp(x) + 1)
+            @!r add.f32        ${out_reg}, ${out_reg}, 1.0;
+            @!r lg2.approx.ftz.f32 ${out_reg}, ${out_reg};
+    """
+    out_str = ""
+
+    for i in range(num_pack):
+        out_str += """
+        {
+        """ + template.format(out_reg=i, in_reg=i + num_pack) + """
+        }
+        """
+    print(out_str)
+    return out_str
+
+def _generate_constraints(num_pack):
+    return ','.join("=r" for i in range(num_pack)) + "," +','.join('r' for i in range(num_pack))
+
+NUM_REG: tl.constexpr = 256
+asm_str: tl.constexpr = _generate_asm(NUM_REG)
+constraints_str: tl.constexpr = _generate_constraints(NUM_REG)
+@triton.jit
+def softplus(x):
+    # out = tl.where(x < 15., tl.math.log2(1 + tl.math.exp2(x)), x)
+    # out = tl.maximum(0, x)
+    out = tl.inline_asm_elementwise(
+        asm=asm_str,
+        constraints=constraints_str,
+        pack=NUM_REG,
+        args=[x,],
+        dtype=tl.float32,
+        is_pure=True,
+    ) 
+    return out
